931c5259844968af50175497eec20de114bf01ea
Tony Tziorvas <tonytziorvas@gmail.com>
2024-06-28 00:21:39 +0300

    final working state

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 const UserManagement = artifacts.require('UserManagement');
 const ProductManagement = artifacts.require('ProductManagement');
 const ShippingManagement = artifacts.require('ShippingManagement');
-const SupplyChain = artifacts.require('SupplyChain');
+// const SupplyChain = artifacts.require('SupplyChain');
 
 module.exports = async function (deployer, network, accounts) {
     // Deploy UserManagement
@@ -17,16 +17,16 @@ module.exports = async function (deployer, network, accounts) {
     const shippingManagement = await ShippingManagement.deployed();
 
     // Deploy SupplyChain with the addresses of the above contracts
-    await deployer.deploy(
-        SupplyChain,
-        userManagement.address,
-        productManagement.address,
-        shippingManagement.address,
-    );
-    const supplyChain = await SupplyChain.deployed();
+    // await deployer.deploy(
+    //     SupplyChain,
+    //     userManagement.address,
+    //     productManagement.address,
+    //     shippingManagement.address,
+    // );
+    // const supplyChain = await SupplyChain.deployed();
 
     console.log('UserManagement deployed at:', userManagement.address);
     console.log('ProductManagement deployed at:', productManagement.address);
     console.log('ShippingManagement deployed at:', shippingManagement.address);
-    console.log('SupplyChain deployed at:', supplyChain.address);
+    // console.log('SupplyChain deployed at:', supplyChain.address);
 };
